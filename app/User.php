d73f0bd9f327acdaf4b3ddff25908da88b35ad20
Mohammed Hussein <hack.mhd@gmail.com>
2018-08-24 18:10:47 +0300

    JWT auth

--- app/User.php
+++ app/User.php
@@ -5,7 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Model\Question;
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -30,5 +32,25 @@ class User extends Authenticatable
     public function question(){
         return $this->hasMany(Question::class);
 
+    }
+
+      /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
